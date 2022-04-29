727ed2f1d9973f08d5334ea17bf4d5cb23c05842
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-29 15:35:05 +0900

    create : create score models

---
+++ database/models/hitter_score.py
@@ -0,0 +1,42 @@
+from ast import For
+from numbers import Integral
+from database.models import Base
+from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
+
+
+class HitterScore(Base):
+    __tablename__ = 'hitter_score'
+
+    score_id = Column(String(50), primary_key=True)
+    
+    date = Column(Date(), nullable=False)
+    date_int = Column(Integer(10), nullable=False)
+
+    # 경기, 타석, 타수
+    game_count = Column(Integer())
+    plate_appearance = Column(Integer())
+    time_at_bat = Column(Integer())
+
+    # 안타, 2루타, 3루타, 홈런
+    hit = Column(Integer())
+    double = Column(Integer())
+    triple = Column(Integer())
+    homerun = Column(Integer())
+
+    # 타점, 득점
+    runs_batted_in = Column(Integer())
+    runs = Column(Integer())
+
+    # 도루, 사사구, 삼진
+    stolen_base = Column(Integer())
+    base_on_ball = Column(Integer())
+    strikeouot = Column(Integer())
+
+    # 타율, 출루율, 장타율, ops
+    batting_average = Column(Float())
+    on_base_percent = Column(Float())
+    sluggin_percentage = Column(Float())
+    ops = Column(Float())
+
+    # Foreign Key
+    fk_player_id = Column(String(50), ForeignKey("player.player_id"))
---
+++ database/models/pitcher_score.py
@@ -0,0 +1,43 @@
+from database.models import Base
+from sqlalchemy import Column, Date, Float, Integer, String, ForeignKey
+
+
+class PitcherScore(Base):
+    __tablename__ = 'pitcher_score'
+
+    score_id = Column(String(50), primary_key=True)
+    
+    date = Column(Date(), nullable=False)
+    date_int = Column(Integer(10), nullable=False)
+
+    # 경기, 승, 패, 세이브, 홀드
+    game_count = Column(Integer())
+    win = Column(Integer())
+    loss = Column(Integer())
+    save = Column(Integer())
+    hold = Column(Integer())
+
+    # 이닝(자연수부분), 이닝(분수부분), 투구수
+    innings_pitched_natural = Column(Integer())
+    innings_pitched_fraction = Column(String())
+    number_of_pitches = Column(Integer())
+
+    # 피안타, 피홈런
+    hit = Column(Integer())
+    homerun = Column(Integer())
+
+    # 탈삼진, 사사구, 실점
+    strike_out = Column(Integer())
+    base_on_balls = Column(Integer())
+    run = Column(Integer())
+
+    # 자책, 평균자책, whip
+    earned_run = Column(Integer())
+    era = Column(Float())
+    whip = Column(Float())
+
+    # qs
+    quality_start = Column(Integer())
+
+    # Foreign Key
+    fk_player_id = Column(String(50), ForeignKey("player.player_id"))
